ceb84788696d2bdf2ec63ce9632fdcda629851a4
Joe <joegbo247@gmail.com>
2021-12-28 14:20:06 +0100

    third commit

---
+++ header.php
@@ -0,0 +1,121 @@
+<html lang="en">
+
+<head>
+   <!-- Basic Page Needs ================================================== -->
+   <meta charset="utf-8">
+
+   <!-- Mobile Specific Metas ================================================== -->
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+   <!-- Site Title -->
+   <title>Exhibit - Conference &amp; Event HTML Template</title>
+
+      <!-- CSS
+         ================================================== -->
+   <!-- Bootstrap -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+
+   <!-- FontAwesome -->
+   <link rel="stylesheet" href="css/font-awesome.min.css">
+   <!-- Animation -->
+   <link rel="stylesheet" href="css/animate.css">
+   <!-- magnific -->
+   <link rel="stylesheet" href="css/magnific-popup.css">
+   <!-- carousel -->
+   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <!-- isotop -->
+   <link rel="stylesheet" href="css/isotop.css">
+   <!-- ico fonts -->
+   <link rel="stylesheet" href="css/xsIcon.css">
+   <!-- Template styles-->
+   <link rel="stylesheet" href="css/style.css">
+   <!-- Responsive styles-->
+   <link rel="stylesheet" href="css/responsive.css">
+
+   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+</head>
+
+<body>
+   <div class="body-inner">
+      <!-- Header start -->
+      <header id="header" class="header header-transparent">
+         <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+               <!-- logo-->
+               <a class="navbar-brand" href="index.html">
+                  <img src="images/logos/logo.png" style="width:151px !important; height:71px; !important" alt="">
+               </a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"><i class="icon icon-menu"></i></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav ml-auto">
+                     <li class="dropdown nav-item active">
+                        <a href="#" class="" data-toggle="dropdown">Home <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                              <li><a href="index.html">Home One</a></li>
+                              <li><a href="index-2.html">Home Two</a></li>
+                              <li><a href="index-3.html">Home Three</a></li>
+                              <li><a href="index-4.html">Home Four</a></li>
+                              <li><a href="index-5.html">Home Five</a></li>
+                              <li><a href="index-6.html">Home Six</a></li>
+                              <li><a href="index-7.html">Home Seven</a></li>
+                        </ul>
+                     </li>
+                     <li class="dropdown nav-item">
+                        <a href="#" class="" data-toggle="dropdown">About <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                           <li><a href="about-us.html">About Us</a></li>
+                           <li><a href="gallery.html">Gallery</a></li>
+                           <li><a href="faq.html">FAQ</a></li>
+                           <li><a href="pricing.html">Pricing Table</a></li>
+                           <li><a href="sponsors-1.html">Sponsors</a></li>
+                           <li><a href="venue.html">Venue</a></li>
+                           <li><a href="404.html">Erro Page</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a href="#" class="" data-toggle="dropdown">Speakers <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                           <li><a href="speakers-1.html">Speakers-1</a></li>
+                           <li><a href="speakers-2.html">Speakers-2</a></li>
+                        </ul>
+
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a href="#" class="" data-toggle="dropdown">Schedule <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                              <li><a href="schedule-list.html">Schedule List</a></li>
+                              <li><a href="schedule-tab-1.html">Schedule Tab 1</a></li>
+                              <li><a href="schedule-tab-2.html">Schedule Tab 2</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a href="#"> Blog <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                           <li><a href="blog.html">Blog</a></li>
+                           <li><a href="news-single.html">Blog Details</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item">
+                        <a href="contact.html">Contact</a>
+                     </li>
+                     <li class="header-ticket nav-item">
+                        <a class="ticket-btn btn" href="pricing.html"> Buy  Ticket
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </nav>
+         </div><!-- container end-->
+      </header>
+      <!--/ Header end -->
+
